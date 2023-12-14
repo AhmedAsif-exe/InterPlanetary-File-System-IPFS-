05cab048a3f40c2ecd478e7edeb581bdd32472d4
Ahmed Asif <ahmedaanasif@gmail.com>
2023-12-14 20:00:50 +0500

    File successfully Ported

--- File.h
+++ File.h
@@ -5,42 +5,43 @@ struct File {
 	std::string Hash;
 	std::string Path;
 	File* next;
-	File(std::string path, std::string hash ) {
+	File(std::string path, std::string hash) {
 		Path = path;
 		Hash = hash;
 		next = nullptr;
 	}
 
-	bool operator>( File& file) {
+	bool operator>(File& file) {
 		return Hash > file.Hash;
 	}
 	bool operator>(std::string hash) {
 		return Hash > hash;
 	}
-	bool operator<( File& file) {
+	bool operator<(File& file) {
 		return Hash < file.Hash;
-	}bool operator<( std::string hash) {
+	}bool operator<(std::string hash) {
 		return Hash < hash;
 	}
-	bool operator == ( File& file) {
+	bool operator == (File& file) {
 		return Hash == file.Hash;
 	}
 	bool operator ==(std::string hash) {
 		return Hash == hash;
 	}
-	bool operator <= ( File& file) {
+	bool operator <= (File& file) {
 		return Hash <= file.Hash;
 	}
 	bool operator <= (std::string hash) {
 		return Hash <= hash;
 	}
-	bool operator >= ( File& file) {
+	bool operator >= (File& file) {
 		return Hash >= file.Hash;
 	}
 	bool operator >= (std::string hash) {
 		return Hash >= hash;
 	}
-	friend std::ostream& operator<<(std::ostream& os,const File& obj) {
+	friend std::ostream& operator<<(std::ostream& os, const File& obj) {
+		os << obj.Hash << " " << obj.Path;
 		return os;
 	}
 
